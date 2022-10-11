e17f714c459e47dc4055ed87e3e69c65ba65f44a
chun <lamchunyu00@gmail.com>
2022-10-11 10:00:58 +0800

    Update checkstyle

--- src/main/java/seedu/address/model/task/UniqueTaskList.java
+++ src/main/java/seedu/address/model/task/UniqueTaskList.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import seedu.address.model.student.exceptions.DuplicateStudentException;
-import seedu.address.model.student.exceptions.StudentNotFoundException;
 import seedu.address.model.task.exceptions.DuplicateTaskException;
 import seedu.address.model.task.exceptions.TaskNotFoundException;
 
